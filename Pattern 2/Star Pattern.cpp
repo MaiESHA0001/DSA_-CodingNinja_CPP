908cec0005e22a83e743add1df9b075fe88374be
eshu <124776550+MaiESHA0001@users.noreply.github.com>
2024-01-03 17:22:45 +0530

    Create Star Pattern.cpp

---
+++ Pattern 2/Star Pattern.cpp
@@ -0,0 +1,66 @@
+Problem statement
+Print the following pattern
+
+Pattern for N = 4
+
+
+
+
+Hint
+As taught in the video, you just have to modify the code so that instead of printing numbers, it should output stars ('*').
+The dots represent spaces.
+
+
+
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+0 <= N <= 50
+Sample Input 1 :
+3
+Sample Output 1 :
+   *
+  *** 
+ *****
+Sample Input 2 :
+4
+Sample Output 2 :
+    *
+   *** 
+  *****
+ *******
+#include<iostream>
+using namespace std;
+int main()
+{
+int n, s, i, j;
+cin>>n;
+for(i = 1; i <= n; i++)
+{
+
+for(s = i; s < n; s++)
+
+/*
+  #include<iostream>
+using namespace std;
+int main()
+{
+int n, s, i, j;
+cin>>n;
+for(i = 1; i <= n; i++)
+{
+
+for(s = i; s < n; s++)
+{
+cout<<" ";
+}
+
+for(j = 1; j <= 2 * i - 1; j++)
+{
+cout<<"*";
+}
+
+cout<<endl;
+}
+}
+*/
