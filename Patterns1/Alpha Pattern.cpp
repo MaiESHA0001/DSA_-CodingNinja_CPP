227679adeddbce1ea38988ec7f5affaf1c88830e
eshu <124776550+MaiESHA0001@users.noreply.github.com>
2024-01-03 19:02:38 +0530

    Update Alpha Pattern.cpp

--- Patterns1/Alpha Pattern.cpp
+++ Patterns1/Alpha Pattern.cpp
@@ -1,3 +1,4 @@
+/*
 Problem statement
 Print the following pattern for the given N number of rows.
 
@@ -38,7 +39,7 @@ int main(){
      */
     int n;
 
-/*
+*/
 #include<iostream>
 using namespace std;
 
@@ -64,4 +65,4 @@ int main(){
     
     }
 }
-*/
+
