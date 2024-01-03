07c22b49bc7c41c5658ca74329118519c0286ac6
eshu <124776550+MaiESHA0001@users.noreply.github.com>
2024-01-03 21:51:32 +0530

    Create Selection Sort.cpp

---
+++ Searching And Sorting/Selection Sort.cpp
@@ -0,0 +1,26 @@
+/*
+#include<iostream>
+using namespace std;
+
+#include"solution.h"
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int left = 0;
+    int right =n-1;
+    int target;
+    cin>>target;
+    int ans = binarySearchClosest(arr, 0, n-1, target);
+    cout<<ans;
+    return 0;
+}
+*/
+
+
+
+when the input is very small and simple
