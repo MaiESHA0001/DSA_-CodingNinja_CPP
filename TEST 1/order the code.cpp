ac3c70192a595597360accb591eb6b706b4d9469
eshu <124776550+MaiESHA0001@users.noreply.github.com>
2024-01-03 19:50:21 +0530

    Create order the code.cpp

---
+++ TEST 1/order the code.cpp
@@ -0,0 +1,15 @@
+/*
+  Problem statement
+Write the correct order(s) of statements.
+
+We want to print "Coding Ninjas" 10 times.
+
+1.  while(x <= 10) {
+2.  int x = 1;
+3.  x += 1;
+4.  cout<<"Coding Ninjas"<<endl;
+5.  }
+
+*/
+
+                                             21345
