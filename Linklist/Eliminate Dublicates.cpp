ac449a84643297c7a97c7a7a47df7a5752782756
eshu <124776550+MaiESHA0001@users.noreply.github.com>
2024-01-08 23:42:31 +0530

    Create Eliminate Dublicates.cpp

---
+++ Linklist/Eliminate Dublicates.cpp
@@ -0,0 +1,84 @@
+Problem statement
+You have been given a singly linked list of integers where the elements are sorted in ascending order. Write a function that removes the consecutive duplicate values such that the given list only contains unique elements and returns the head to the updated list.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1 <= t <= 10^2
+0 <= M <= 10^5
+Time Limit: 1sec
+
+Where 'M' is the size of the singly linked list.
+ Remember/Consider :
+While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element.
+Sample Input 1 :
+1
+1 2 3 3 3 3 4 4 4 5 5 7 -1
+Sample Output 1 :
+1 2 3 4 5 7 
+Sample Input 2 :
+2
+10 20 30 40 50 -1
+10 10 10 10 -1
+Sample Output 2 :
+10 20 30 40 50
+10
+/****************************************************************
+ 
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+
+
+
+
+
+
+
+
+ Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+Node *removeDuplicates(Node *head)
+{
+    //Write your code here
+	   if(head == NULL) {
+        return head;
+    }
+    
+    Node *curr = head -> next;
+    Node *ptr = head;
+    
+    while(curr){
+        if(curr -> data == ptr -> data) {
+            if(curr -> next == NULL) {
+                ptr -> next = NULL;
+            }
+                curr = curr -> next;
+        } else {
+        	ptr -> next = curr;
+        	ptr = curr;
+        	curr = curr -> next;
+        }
+    }
+    
+    return head;
+}
+
