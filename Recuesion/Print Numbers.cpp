052e60692266f282a1c342567ee7d4ea5c418ae8
eshu <124776550+MaiESHA0001@users.noreply.github.com>
2024-02-09 21:54:15 +0530

    Create Print Numbers.cpp

---
+++ Recuesion/Print Numbers.cpp
@@ -0,0 +1,66 @@
+Problem statement
+Given the number 'n', write a code to print numbers from 1 to n in increasing order recursively.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1 <= n <= 10000
+Sample Input 1 :
+ 6
+Sample Output 1 :
+1 2 3 4 5 6
+Sample Input 2 :
+ 4
+Sample Output 2 :
+1 2 3 4
+
+
+
+
+
+  
+
+int main(){
+    int n;
+    cin >> n;
+  
+    print(n);
+}
+
+
+
+
+
+
+
+
+
+void print(int n){
+    //write your code here
+  if (n > 0) {
+        print(n - 1);
+        cout << n << " ";
+    }
+}
+
+
+
+
+
+
+/* mainn code */
+
+
+#include<iostream>
+using namespace std;
+#include "Solution.h"
+
+int main(){
+    int n;
+    cin >> n;
+  
+    print(n);
+}
+
+
+
+  
